1f8c918f96a114c51bfb722abe6eb6d73dd5f880
IMLUOAI <imluoai@gmail.com>
2024-01-29 00:30:26 -0600

    feat/ header part

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,24 @@
-import logo from '../logo.svg';
-import './app';
-
+import "../blocks/app/app.css";
 function App() {
   return (
-    <div className="App">
-<header/>
-<main />
-<itemModal />
-<modalWithForm />
+    <div>
+      <header className="header">
+        <div className="header__logo">
+          <div>
+            <img src="/images/logo.svg" alt="logo" />
+          </div>
+          <div className="header__date">January 27th, Plano </div>
+        </div>
+        <div className="header__avatar-logo">
+          <button className="header__add-button" type="text">
+            +Add clothes
+          </button>
+          <div className="header__user-name">Samuel Luo</div>
+          <div>
+            <img src="/images/avatar.svg" alt="avatar" />
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
