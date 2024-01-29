b2c492ce8454ce481e79e7cc4c4526da52fd1617
IMLUOAI <IMLUOAI@gmail.com>
2024-01-28 21:55:12 -0600

    new structure

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import logo from '../logo.svg';
+import './app';
+
+function App() {
+  return (
+    <div className="App">
+<header/>
+<main />
+<itemModal />
+<modalWithForm />
+    </div>
+  );
+}
+
+export default App;
