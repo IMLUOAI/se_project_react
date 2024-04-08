6351f395d021d7846573b0b2e99f3a2745e681ea
IMLUOAI <imluoai@gmail.com>
2024-04-08 00:53:39 -0500

    fix weathercard.js

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../blocks/weatherCard/weatherCard.css";
+import CurrentTemperatureUnitContext from "../contexts/CurrentTemperatureUnitContext";
 import { weatherOptions } from "../utils/constants";
 
-const WeatherCard = ({ sunny, type, weatherTemp, temperature }) => {
+const WeatherCard = ({ weatherTemp = "", day, type = "sunny" }) => {
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+  console.log("weatehr card");
   const imageSrc = weatherOptions.filter((i) => {
     console.log(i);
-    return i.day === sunny && i.type === type;
+    return i.day === day && i.type === type;
   });
+
   const imageSrcUrl = imageSrc.length > 0 ? imageSrc[0].url : "";
   const altText = imageSrc ? `weather: ${type}` : "weather icon";
   return (
     <section className="weather__bar" id="weather">
-      <h2 className="weather__info">{weatherTemp + "°" + temperature}</h2>
+      <h2 className="weather__info">
+        {weatherTemp}°{currentTemperatureUnit}{" "}
+      </h2>
       <img src={imageSrcUrl} alt={altText} className="weather__image" />
     </section>
   );
