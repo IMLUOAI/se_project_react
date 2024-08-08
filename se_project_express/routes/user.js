3f0ea82172d97b0824360723a91defd89225c7b6
IMLUOAI <IMLUOAI@gmail.com>
2024-08-08 16:20:32 -0500

    Updated the override version

--- se_project_express/routes/user.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { getCurrentUser, updateCurrentUser } = require("../controllers/user");
-
-router.get('/me',  getCurrentUser);
-router.patch('/me', updateCurrentUser);
-
-module.exports = router;
--- se_project_express/utils/config.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET || '66855fdbeb59e5e0524c779d',
-};
